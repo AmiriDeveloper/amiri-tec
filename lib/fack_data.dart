b1e65a1223921ac5ef33da8985c4d701075e7c38
FLAMINGO0STARS <flamingo6009@gmail.com>
2022-05-31 15:23:21 +0430

    lala

---
+++ lib/fack_data.dart
@@ -0,0 +1,8 @@
+
+Map homePagePosterMap = {
+  "imageAsset ": "assets/images/skynews.jpg",
+  "writer": "ملیکا عزیزی",
+  "data": "-یک روز پیش",
+  "title": "دوازده قدم برنامه نویسی یک دوره ی...",
+  "view": "250"
+};
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,20 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tec/gen/assets.gen.dart';
+import 'package:tec/fack_data.dart';
 import 'package:tec/my_colors.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     var theme = Theme.of(context).textTheme;
     var size = MediaQuery.of(context).size;
-
 
     // TODO: implement build
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(0,16,0,0),
+        padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
         child: SafeArea(
           child: Column(
             children: [
@@ -30,41 +27,51 @@ class MainScreen extends StatelessWidget {
                   Icon(Icons.search),
                 ],
               ),
-              SizedBox(  height: 8,),
-                 Stack(
-                  children: [
-                    Container(
-                      width: size.width/1.20,
-                      height: size.height/4.2,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
-                          image: DecorationImage(
-                              image: Image(
-                                      image:
-                                          AssetImage('assets/images/skynews.jpg'))
-                                  .image,
-                              fit: BoxFit.cover)),
-                      foregroundDecoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
-                          gradient: LinearGradient(
-                              colors: gradiantColrs.posterCoverGradiant,
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter)),
-                    ),
-
-                    Positioned(left: 0,right: 0,bottom: 8,
-                      child: Column(children: [
+              SizedBox(
+                height: 8,
+              ),
+              Stack(
+                children: [
+                  Container(
+                    width: size.width / 1.20,
+                    height: size.height / 4.2,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                        image: DecorationImage(
+                            image: AssetImage(homePagePosterMap["imageAsset"]),
+                            fit: BoxFit.cover)),
+                    foregroundDecoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                        gradient: LinearGradient(
+                            colors: gradiantColrs.posterCoverGradiant,
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter)),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: 8,
+                    child: Column(
+                      children: [
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-
-                          Text('ملیکا عزیزی - یک وز پیش ',style: theme.subtitle1),
-                          Text('Like 253',style: theme.subtitle1),
-                        ],), Text('دوازده قدم برنامه نویسی یک دوره ی...',style: theme.headline1),
-                      ],),
-                    )
-                  ],
-                ),
+                            Text(
+                                homePagePosterMap["writer"] +
+                                    " " +
+                                    homePagePosterMap["data"],
+                                style: theme.subtitle1),
+                            Text(homePagePosterMap["view"],
+                                style: theme.subtitle1),
+                          ],
+                        ),
+                        Text(homePagePosterMap["title"],
+                            style: theme.headline1),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
